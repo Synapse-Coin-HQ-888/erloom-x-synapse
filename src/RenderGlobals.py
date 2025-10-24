01f695e6bf5351fb06489e18261b8ec01e03ee29
Synapse Coin <groyxerz700@gmail.com>
2025-10-24 19:03:00 +0300

    Create RenderGlobals.py

---
+++ src/RenderGlobals.py
@@ -0,0 +1,9 @@
+from src.renderer import Renderer
+from errloom.utils.signal_block import RenderVars
+from errloom.session import Session
+from src.hud import HUD
+
+renderer:Renderer = Renderer.null
+rv: RenderVars = RenderVars.null
+session: Session = Session.null
+hud:HUD = HUD.null
